c30ce6111371bccefa277b3bd849b30658278a15
AkashDeep000 <akashdeepdas970@gmail.com>
2024-02-19 22:08:36 +0530

    bug fix

--- src/utils/getPages.ts
+++ src/utils/getPages.ts
@@ -17,22 +17,13 @@ const getPages = ({
       ? (chemistry as Question[])
       : (physics as Question[]);
 
-  const filteredData = data.filter(qn => {
-    if (classYear.split('-')[1] == "22") {
-      return qn.ncert22_page.split('-')[0] ==  classYear.split('-')[0]
-    }
-    if (classYear.split('-')[1] == "23") {
-      return qn.ncert23_page.split('-')[0] ==  classYear.split('-')[0]
-    }
-  })
-  
-  const onlyMcq = filteredData.filter((qn) => qn.quiz_type === "mcq");
+  const onlyMcq = data.filter((qn) => qn.ncert22_page &&qn.quiz_type === "mcq");
   const pagesMap = new Map<string, { total: number; ids: string[] }>();
 
   for (const qn of onlyMcq) {
-    if (classYear.split('-')[1] == "22") {
-      const page = qn.ncert22_page.split('-')[1]
-      if (page && qn.ncert22_page.split("-")[0] == classYear.split('-')[0]) {
+    if (qn.ncert23_page?.length > 0 && classYear.split("-")[1] == "22") {
+      const page = qn.ncert22_page.split("-")[1];
+      if (page && qn.ncert22_page.split("-")[0] == classYear.split("-")[0]) {
         const currentIds = pagesMap.get(qn.unique_id)?.ids || [];
         pagesMap.set(page, {
           total: (pagesMap.get(page)?.total || 0) + 1,
@@ -40,21 +31,27 @@ const getPages = ({
         });
       }
     }
-    if (classYear.split('-')[1] == "23") {
-      const page = qn.ncert23_page.split('-')[1]
-      if (page && qn.ncert23_page.split("-")[0] == classYear.split('-')[0]) {
+    if (qn.ncert23_page?.length > 0 && classYear.split("-")[1] == "23") {
+      const page = qn.ncert23_page.split("-")[1];
+      if (page && qn.ncert23_page.split("-")[0] == classYear.split("-")[0]) {
         const currentIds = pagesMap.get(qn.unique_id)?.ids || [];
         pagesMap.set(page, {
           total: (pagesMap.get(page)?.total || 0) + 1,
           ids: [...currentIds, qn.unique_id],
         });
       }
-    }  
+    }
   }
 
-  return [...pagesMap].map(([name, value]) => {
-    return { name: parseInt(name).toString(), total: value.total, ids: value.ids };
-  }).sort((a,b) => parseInt(a.name) - parseInt(b.name));
+  return [...pagesMap]
+    .map(([name, value]) => {
+      return {
+        name: parseInt(name).toString(),
+        total: value.total,
+        ids: value.ids,
+      };
+    })
+    .sort((a, b) => parseInt(a.name) - parseInt(b.name));
 };
 
 export default getPages;

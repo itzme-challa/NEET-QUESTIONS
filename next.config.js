5c6938a8f9029a9d96a54460c3d7a072d841abb1
AkashDeep000 <akashdeepdas970@gmail.com>
2024-02-20 01:06:30 +0530

    offline fallback page

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const withPWA = require("@ducanh2912/next-pwa").default({
     workboxOptions: {
       disableDevLogs: true,
     },
+    fallbacks: {
+      document: "/offline"
+    }
     // ... other options you like
   });
   

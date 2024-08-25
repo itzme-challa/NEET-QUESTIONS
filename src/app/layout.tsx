9dcf2fa779c97d6e1f56084db3e7a20d9707b1cb
Akashdeep <76022812+AkashDeep000@users.noreply.github.com>
2024-08-26 02:09:57 +0530

    Fix: Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,12 +42,8 @@ export default function RootLayout({
           <SiteHeader></SiteHeader>
           <main>{children}</main>
         </ThemeProvider>
+        <Script src="https://umami.akashdeep.pro/script.js" data-website-id="f51d55ab-4933-4024-b1f8-6badd9d7362f" />
         <Script src="https://umami.coolify.akashdeep.pro/script.js" data-website-id="689bb58a-6b31-44a9-b3bf-1f7756af6c42" />
-        <Script data-domain="neet-prep.vercel.app" src="https://plausible.io/js/script.js" />
-        <script>
-            !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init push capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey getNextSurveyStep identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-            posthog.init('phc_IVOb2fBD12MvbDNCWLWEhuDKXPJLSEvvctMFpSD2R0a',{api_host: 'https://us.i.posthog.com', person_profiles: 'always'})
-        </script>
       </body>
     </html>
   );

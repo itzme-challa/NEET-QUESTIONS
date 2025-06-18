4161c48e6e08e756b9af8adc4184e60645f141e7
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-06-18 21:15:01 +0530

    Update and rename env.d.ts to index.js

--- env.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-  export interface ProcessEnv {
-    DB_URL: string;
-    AUTH_GITHUB_ID: string;
-    AUTH_GITHUB_SECRET: string;
-    AUTH_SECRET: string;
-    ENVRIONMENT: "development" | "production";
-  }
-}

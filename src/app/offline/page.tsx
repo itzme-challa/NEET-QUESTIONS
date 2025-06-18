0d3d7b4c732908d812e0e3c937572605e8829155
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-06-18 21:18:34 +0530

    Delete src directory

--- src/app/offline/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Home() {
-  return (
-    <div className="mx-auto w-full max-w-2xl p-4">
-    <p className="py-16 px-8 text-center text-lg">You are currently offline. Please connect to internet.</p>
-    </div>
-  );
-}

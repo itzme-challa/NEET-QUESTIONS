9042fe0393393a79ff9bf5e10f94064f7ffcf141
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-07-09 23:06:10 +0530

    Update and rename tests.html to tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1976D2',
+        secondary: '#FF9800'
+      }
+    }
+  },
+  plugins: []
+}

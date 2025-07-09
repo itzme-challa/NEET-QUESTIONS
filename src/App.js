f0e48a2fd11d52dc5180b55260a14bcc5b86578c
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-07-09 23:47:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
 import Play from './play';
 import Results from './results';
 
@@ -10,24 +11,8 @@ function App() {
     <Router>
       <div className="min-h-screen bg-gray-100 font-inter">
         <Routes>
-          <Route
-            path="/"
-            element={
-              quizStarted ? (
-                <Play setQuizStarted={setQuizStarted} />
-              ) : (
-                <div className="flex items-center justify-center h-screen">
-                  <button
-                    onClick={() => setQuizStarted(true)}
-                    className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition"
-                    id="start-btn"
-                  >
-                    Start Quiz
-                  </button>
-                </div>
-              )
-            }
-          />
+          <Route path="/" element={<Home setQuizStarted={setQuizStarted} />} />
+          <Route path="/play" element={<Play setQuizStarted={setQuizStarted} quizStarted={quizStarted} />} />
           <Route path="/results" element={<Results />} />
         </Routes>
       </div>

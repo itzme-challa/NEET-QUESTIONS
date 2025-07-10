d9fb0045f7143885cad412d53d31931dee713fe2
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-07-10 18:29:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import Play from './play'; // Update to './Play' if renaming the file
-import Results from './results';
+import Play from './Play';
+import Results from './Results';
 
 function App() {
-  const [quizStarted, setQuizStarted] = useState(false);
-
   return (
     <Router>
       <div className="min-h-screen bg-gray-100 font-inter">
         <Routes>
-          <Route path="/" element={<Home setQuizStarted={setQuizStarted} />} />
-          <Route path="/play" element={<Play setQuizStarted={setQuizStarted} quizStarted={quizStarted} />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/play" element={<Play />} />
           <Route path="/results" element={<Results />} />
         </Routes>
       </div>

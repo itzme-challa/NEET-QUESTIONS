5a5c177ac4d43adc48f72d1d92a87cb9e08e5178
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-07-11 10:34:48 +0530

    Update and rename index.html to _app.js

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
